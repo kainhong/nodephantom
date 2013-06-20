175efd839ad467f76b543d1d0a25f767fb46415c
xiaodoudoudou8 <liling.hg@gmail.com>
2013-06-20 17:51:07 +0800

    add image and clean button

--- node_web/nodejs-demo/routes/render.js
+++ node_web/nodejs-demo/routes/render.js
@@ -1,48 +1,62 @@
+//path = require('path');
+var moment = require('moment');
 var mongoose = require('mongoose');
 var SiteSchema = new mongoose.Schema({
     SiteID: { type: Number, required: false },
-    SiteUrl: { type: String, required: false }
+    SiteUrl: { type: String, required: false },
+    SiteTitle: { type: String, required: false },
+    SiteScreenshotFileName: { type: String, required: false }
 });
 var SiteModel = mongoose.model('Site', SiteSchema);
 var phantom = require('node-phantom');
+var date = date()
 exports.list = function(req, res){
     var Site1;
+    Site1 = new SiteModel({
+        SiteUrl: req.body.fetchUrl,
+        SiteScreenshotFileName: imagename
+    });
     console.log("POST(render.js):");
     console.log('req.body: '+ req.body.fetchUrl);
     if(req.body.fetchUrl == ''){
         res.send(500, { error: 'something blew up' });
     }else{
-        try{phantom.create(function(err, ph) {
-            console.log('notice:renderPage function is work');
-            ph.createPage(function(err, page) {
-                page.open(req.body.fetchUrl, function(err, status) {
-                    console.log('opened '+ req.body.fetchUrl +'?', status);
-                    page.render('godddggle.png');
-                    page.includeJs('http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js', function() {
-                        setTimeout(function(){
-                            page.evaluate(function(){
-                                var title;
-                                title = $('title').html();
-                                //return title
-                            },function(err, result){
-                                console.log(result);
-                            });
-                        },5000);
+        try{
+            phantom.create(function(err, ph) {
+                ph.createPage(function(err, page) {
+                    page.open(req.body.fetchUrl, function(err, status) {
+                        console.log('opened '+ req.body.fetchUrl +'?', status);
+                        //page.render('./touzi101/godddggle.png');
+                        Site1.SiteScreenshotFileName = 'godddggle.png';
+                        page.includeJs('http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js', function() {
+                            
+                                Site1 = page.evaluate(function(pagesource){
+
+                                    pagesource.SiteTitle = $('title').html();
+                                    return pagesource
+                                },function(err, result){
+                                    if (!err){
+                                        console.log(result);
+                                    }
+                                    
+                                }, Site1);
+                            
+                        });
                     });
                 });
+                //ph.exit();
             });
-            //ph.exit();
-        });}
+            console.log('pagesource: ' + Site1.SiteTitle + ' ssname ' + Site1.SiteScreenshotFileName);
+        }
         catch(error){
             console.log("This is never printed."+error);
         }
-        Site1 = new SiteModel({
-            SiteUrl: req.body.fetchUrl
-        });
+
         Site1.save(function (err) {
             if (!err) {
                 SiteModel.findOne( {'SiteUrl':req.body.fetchUrl},function (err, Sites) {
                     if (!err) {
+                        console.log('one record of :' + Sites );
                         res.send(200,Sites);
                     } else {
                         res.send(500, { error: 'something blew up' });
@@ -71,7 +85,10 @@ exports.fetchdata = function(req, res){
         }
     });
 };
-
+exports.cleandatas = function(req, res){
+    SiteModel.find().remove();
+    res.send(200);
+};
 
 function renderPage(url){
     // file name: phantomTest.js
